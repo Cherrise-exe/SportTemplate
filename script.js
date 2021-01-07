3cd8211c390139e84159a746215b48a19972fea5
Cherrise Hatcher <cherrisehatcher@gmail.com>
2021-01-07 11:41:21 -0500

    images and quotes

---
+++ script.js
@@ -0,0 +1,59 @@
+/*
+unofficial breakdown
+
+nav bar - with add cart
+
+slideshow - 
+
+https://unsplash.com/photos/4qSb_FWhHKs
+https://unsplash.com/photos/eyhC3UQHQgc
+https://unsplash.com/photos/-PrinhBqyLI
+https://unsplash.com/photos/GSwMQzHUEXk
+https://unsplash.com/photos/NoVnXXmDNi0
+https://unsplash.com/photos/Mtc6BP1WofU
+drop down nav option
+
+pop up promo for membership???
+
+testimonies
+"Amazing shoes take you amazing places."
+"I still have my feet on the ground, I just wear better shoes." - Oprah Winfrey
+"Shoes that can take you anywhere."
+"With the right equipement and mindset, anything is possible."
+"Look in the Mirror... That's your competition."
+"If you can't outplay them, outwork them."
+"Hard work beats talent when talent doesn't work hard."
+
+and full body photos
+https://unsplash.com/photos/DA0_b8GNc10
+https://unsplash.com/photos/yhQhvK04QPc
+https://unsplash.com/photos/mQBB3DHzudE
+https://unsplash.com/photos/0XmvU0DLTxU
+https://unsplash.com/photos/kaZ6Uu54ZjE
+https://unsplash.com/photos/IRG0n9Px6lM
+
+Arrow icons for new arrivals slideshow
+
+New Arrivals - Shoes
+https://unsplash.com/photos/EfLwt5Xz5Ek
+https://unsplash.com/photos/PqbL_mxmaUE
+https://unsplash.com/photos/ad047kyPBh8
+https://unsplash.com/photos/LxVxPA1LOVM
+https://unsplash.com/photos/KvRy5S8hKPw
+https://unsplash.com/photos/164_6wVEHfI
+https://unsplash.com/photos/CMGKxMmZcRk
+https://unsplash.com/photos/-aCtb_RTwuM
+
+New Arrivals - Clothes
+https://unsplash.com/photos/gTTtXwqmKPQ
+https://unsplash.com/photos/89poi2aVXX0
+https://unsplash.com/photos/3DQF5bGjQq4
+https://unsplash.com/photos/vXNeKPih4y0
+https://unsplash.com/photos/JfN0kpooAyw
+https://unsplash.com/photos/_J3X-g-2KKc
+https://unsplash.com/photos/kFtTXa5ZS1Q
+https://unsplash.com/photos/xrvWzRVwAmM
+https://unsplash.com/photos/xXl8swWl9Q4
+https://unsplash.com/photos/QzzdRB5Kc2E
+
+*/
